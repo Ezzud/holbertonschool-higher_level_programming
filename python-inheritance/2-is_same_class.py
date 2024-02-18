ef3ac6642051b4fc3a9ac83095c9c30baa1cc114
Ezzud <contact@ezzud.fr>
2024-02-18 11:18:34 -0800

    Fix projects pycodestyles

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-    """ Function that returns True or False 
+    """ Function that returns True or False
         if object type is a_class
     """
     return type(obj) is a_class
